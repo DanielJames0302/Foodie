8b15e01878b17499036f05d937454cf5b4ead383
mman <minhmanuni@gmail.com>
2024-07-07 14:20:20 +0700

    Create menu page

--- client/src/components/share/Share.tsx
+++ client/src/components/share/Share.tsx
@@ -6,6 +6,7 @@ import { useContext, useState } from "react";
 import { AuthContext } from "../../context/authContext";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { makeRequest } from "../../axios";
+import { Button, ButtonGroup, Dropdown } from "react-bootstrap";
 const Share = () => {
   const [file, setFile] = useState<File>();
   const [desc, setDesc] = useState("");
@@ -14,7 +15,7 @@ const Share = () => {
     try {
       const formData = new FormData();
       formData.append("file", file);
-      console.log(formData.get("file"))
+      console.log(formData.get("file"));
       const res = await makeRequest.post("/upload", formData);
       return res.data;
     } catch (err) {
@@ -30,81 +31,93 @@ const Share = () => {
     mutationFn: (newPost: any) => {
       return makeRequest.post("/posts", newPost);
     },
-    
+
     onSuccess: () => {
-        // Invalidate and refetch
-        queryClient.invalidateQueries({queryKey: ["posts"]});
+      // Invalidate and refetch
+      queryClient.invalidateQueries({ queryKey: ["posts"] });
     },
-    
   });
 
   const selectFile = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { files } = event.target;
     const selectedFiles = files as FileList;
     setFile(selectedFiles?.[0]);
-
   };
 
-  const handleClick = async (e:  React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  const handleClick = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
     e.preventDefault();
     let imgUrl = "";
     if (file) imgUrl = await upload(file);
     mutation.mutate({ desc, img: imgUrl });
-    console.log(imgUrl) 
+    console.log(imgUrl);
     setDesc("");
     setFile(undefined);
   };
- 
 
   return (
-    <div className="share">
-      <div className="container">
-        <div className="top">
-          <div className="left">
-            <img src={currentUser.profilePic ? "/uploads/" + currentUser.profilePic : `/images/default-user.jpg`} alt="" />
-            <input
-              type="text"
-              placeholder={`What's on your mind ${currentUser.name}?`}
-              onChange={(e) => setDesc(e.target.value)}
-              value={desc}
-            />
-          </div>
-          <div className="right">
-            {file && (
-              <img className="file" alt="" src={URL.createObjectURL(file)} />
-            )}
-          </div>
-        </div>
-        <hr />
-        <div className="bottom">
-          <div className="left">
-            <input
-              type="file"
-              id="file"
-              style={{ display: "none" }}
-              onChange={selectFile}
-            />
-            <label htmlFor="file">
-              <div className="item">
-                <img src={Image} alt="" />
-                <span>Add Image</span>
+
+        <div className="share">
+          <div className="container">
+            <div className="top">
+              <div className="left">
+                <img
+                  src={
+                    currentUser.profilePic
+                      ? "/uploads/" + currentUser.profilePic
+                      : `/images/default-user.jpg`
+                  }
+                  alt=""
+                />
+                <input
+                  type="text"
+                  placeholder={`What's on your mind ${currentUser.name}?`}
+                  onChange={(e) => setDesc(e.target.value)}
+                  value={desc}
+                />
               </div>
-            </label>
-            <div className="item">
-              <img src={Map} alt="" />
-              <span>Add Place</span>
+              <div className="right">
+                {file && (
+                  <img
+                    className="file"
+                    alt=""
+                    src={URL.createObjectURL(file)}
+                  />
+                )}
+              </div>
             </div>
-            <div className="item">
-              <img src={Friend} alt="" />
-              <span>Tag Friends</span>
+            <hr />
+            <div className="bottom">
+              <div className="left">
+                <input
+                  type="file"
+                  id="file"
+                  style={{ display: "none" }}
+                  onChange={selectFile}
+                />
+                <label htmlFor="file">
+                  <div className="item">
+                    <img src={Image} alt="" />
+                    <span>Add Image</span>
+                  </div>
+                </label>
+                <div className="item">
+                  <img src={Map} alt="" />
+                  <span>Add Place</span>
+                </div>
+                <div className="item">
+                  <img src={Friend} alt="" />
+                  <span>Tag Friends</span>
+                </div>
+              </div>
+              <div className="right">
+                <button onClick={handleClick}>Share</button>
+              </div>
             </div>
-          </div>
-          <div className="right">
-            <button onClick={handleClick}>Share</button>
           </div>
         </div>
-          </div> 
-    </div>
+ 
   );
 };
 
---
+++ client/src/utils/data.ts
@@ -0,0 +1,114 @@
+export const heroData = [
+  {
+    id: 1,
+    category: "icecreams",
+    name: "Icecream",
+    decp: "Chocolate & vanilla",
+    price: "5.25",
+    imageSrc: 'c1.png',
+  },
+  {
+    id: 2,
+    category: "Fruits",
+    name: "Strawberries",
+    decp: "Fresh Strawberries",
+    price: "10.25",
+    imageSrc: 'c1.png',
+  },
+  {
+    id: 3,
+    name: "Chicken Kebab",
+    category: "chicken",
+    decp: "Mixed Kebab Plate",
+    price: "8.25",
+    imageSrc: 'c1.png',
+  },
+  {
+    id: 4,
+    name: "Fish Kebab",
+    category: "fish",
+    decp: "Mixed Fish Kebab",
+    price: "5.25",
+    imageSrc: 'c1.png',
+  },
+  {
+    id: 5,
+    name: "Chicken Kebab",
+    category: "chicken",
+    decp: "Mixed Kebab Plate",
+    price: "8.25",
+    imageSrc: 'c1.png',
+  },
+  {
+    id: 6,
+    name: "Chicken Kebab",
+    category: "chicken",
+    decp: "Mixed Kebab Plate",
+    price: "8.25",
+    imageSrc: 'c1.png',
+  },
+  {
+    id: 7,
+    name: "Chicken Kebab",
+    category: "chicken",
+    decp: "Mixed Kebab Plate",
+    price: "8.25",
+    imageSrc: 'c1.png',
+  },
+  {
+    id: 8,
+    name: "Chicken Kebab",
+    category: "chicken",
+    decp: "Mixed Kebab Plate",
+    price: "8.25",
+    imageSrc: 'c1.png',
+  },
+  {
+    id: 9,
+    name: "Chicken Kebab",
+    category: "chicken",
+    decp: "Mixed Kebab Plate",
+    price: "8.25",
+    imageSrc: 'c1.png',
+  },
+];
+
+
+export const categories = [
+  {
+    id: 1,
+    name: "Chicken",
+    urlParamName: "chicken",
+  },
+  {
+    id: 2,
+    name: "Curry",
+    urlParamName: "curry",
+  },
+  {
+    id: 3,
+    name: "Rice",
+    urlParamName: "rice",
+  },
+  {
+    id: 4,
+    name: "Fish",
+    urlParamName: "fish",
+  },
+  {
+    id: 5,
+    name: "Fruits",
+    urlParamName: "fruits",
+  },
+  {
+    id: 6,
+    name: "Icecreams",
+    urlParamName: "icecreams",
+  },
+
+  {
+    id: 7,
+    name: "Soft Drinks",
+    urlParamName: "drinks",
+  },
+];
